84378be9b4760e5bc5796d080b9a351559a6a4b8
yoon-chaejin <kevin3st@naver.com>
2025-01-05 14:42:43 +0900

    :heavy_plus_sign:  docs: swagger-ui 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,9 @@ dependencies {
 //	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
+    // API Documentation
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.0")
+
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
