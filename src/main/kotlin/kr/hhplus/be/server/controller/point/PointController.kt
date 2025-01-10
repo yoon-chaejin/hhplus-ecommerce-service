b752f5f7b18047564ceb752620e24d6b2323aa89
yoon-chaejin <kevin3st@naver.com>
2025-01-10 09:22:23 +0900

    :memo: docs: API 문서 보완

--- src/main/kotlin/kr/hhplus/be/server/controller/point/PointController.kt
+++ src/main/kotlin/kr/hhplus/be/server/controller/point/PointController.kt
@@ -1,5 +1,7 @@
 package kr.hhplus.be.server.controller.point
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import kr.hhplus.be.server.common.exception.CustomException
 import kr.hhplus.be.server.common.exception.CustomExceptionType
 import kr.hhplus.be.server.controller.point.model.ChargeRequest
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
+@Tag(name = "포인트 관리")
 @RestController
 class PointController {
 
+    @Operation(summary = "사용자 포인트 충전", description = "사용자의 포인트를 충전한다.")
     @PostMapping("/users/{userId}/points/charge")
     fun charge(@PathVariable userId: Long, @RequestBody request: ChargeRequest): ResponseEntity<ChargeResponse> {
         require(request.amount > 0) { throw CustomException(CustomExceptionType.INVALID_CHARGE_AMOUNT) }
@@ -26,6 +30,7 @@ class PointController {
         ))
     }
 
+    @Operation(summary = "사용자 포인트 조회", description = "사용자의 포인트를 조회한다.")
     @GetMapping("/users/{userId}/points")
     fun getPoints(@PathVariable userId: Long): ResponseEntity<GetPointsResponse> {
         return ResponseEntity.ok(GetPointsResponse(
--- src/main/kotlin/kr/hhplus/be/server/controller/product/ProductController.kt
+++ src/main/kotlin/kr/hhplus/be/server/controller/product/ProductController.kt
@@ -1,5 +1,7 @@
 package kr.hhplus.be.server.controller.product
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import kr.hhplus.be.server.controller.product.model.GetPopularProductsResponse
 import kr.hhplus.be.server.controller.product.model.GetProductsResponse
 import kr.hhplus.be.server.controller.product.model.PopularProductResponse
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "상품 관리")
 @RestController
 class ProductController {
 
+    @Operation(summary = "상품 목록 조회", description = "상품 목록을 조회한다.")
     @GetMapping("products")
     fun getProducts(
         @RequestParam(value = "page", defaultValue = "0") page: Int,
@@ -36,6 +40,7 @@ class ProductController {
         return ResponseEntity.ok(GetProductsResponse(products))
     }
 
+    @Operation(summary = "인기 상품 조회", description = "인기 상품 5건을 조회한다.")
     @GetMapping("/products/popular")
     fun getPopularProducts(): ResponseEntity<GetPopularProductsResponse> {
         val products = ArrayList<PopularProductResponse>()
