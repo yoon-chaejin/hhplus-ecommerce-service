8440d969fedaa02848d03ad648d638670021d583
yoon-chaejin <kevin3st@naver.com>
2025-02-14 05:12:32 +0900

    :bug: bugfix : 불필요한 스케줄러 실행 방지

--- src/main/kotlin/kr/hhplus/be/server/scheduler/IssueCouponScheduler.kt
+++ src/main/kotlin/kr/hhplus/be/server/scheduler/IssueCouponScheduler.kt
@@ -1,12 +1,14 @@
 package kr.hhplus.be.server.scheduler
 
 import kr.hhplus.be.server.application.CouponApplication
+import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
 @EnableScheduling
+@Profile("local", "scheduler")
 class IssueCouponScheduler(
     private val couponApplication: CouponApplication,
 ) {
